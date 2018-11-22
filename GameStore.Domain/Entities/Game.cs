ba04ed63207c7f0068369b4cdf9777be9ff8a3e2
Lecto Anto <sector.zat@gmail.com>
2018-11-23 02:09:21 +0400

    50%

---
+++ GameStore.Domain/Entities/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.Domain.Entities
+{
+    public class Game
+    {
+        public int GameID { get; set; }
+        public String Name { get; set; }
+        public String Descriptions { get; set; }
+        public String Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ GameStore.WebUI/Models/PagingInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GameStore.WebUI.Models
+{
+    public class PagingInfo
+    {
+        //кол-во товаров
+        public int TotalItems { get; set; }
+
+        //кол-во товаров на одной странице
+        public int ItemsPerPage { get; set; }
+
+        //Номер текущей страницы
+        public int CurrentPage { get; set; }
+
+        //Общее кол-во страниц
+        public int TotalPages
+        {
+            get { return (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage); }
+        }
+    }
+}
